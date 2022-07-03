7f7cf4a9e9c733feacbc81c465241981b441c0fe
MirzaAyon <ayonmirza98@gmail.com>
2022-07-04 01:25:48 +0600

    quiz added

---
+++ quiz.js
@@ -0,0 +1,9 @@
+// 6.What is recursion in programming?
+// ans : a function that calls itself and has a termination condition
+
+//10 : Fill the blank for 10 numbers decrementing loop?
+
+// for( let i = 10; ______?_____; i--){
+
+// }
+//i>=1
